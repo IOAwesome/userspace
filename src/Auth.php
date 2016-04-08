9a446098b8892dea6dc8962a089d45b352dc44a5
Grant H <masterofones@outlook.com>
2016-04-08 18:01:34 +0100

    Testing Class instantiation

--- src/Auth.php
+++ src/Auth.php
@@ -3,6 +3,16 @@ namespace ioawesome\userspace;
 
 class Auth
 {
+    public function __construct($args)
+    {
+        echo "<br />Construct";
+        if( count($args) > 0 ) {
+            foreach ($args as $key -> $val) {
+                echo sprintf("Key: %s Val: %s <br />", $key, $val);
+            }
+        }
+    }
+
     public function HelloWorld() {
         echo "This shit is bananas";
     }
