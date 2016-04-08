78b14c0e81507776bec729ece3b1b7e8e2c00a6d
Grant H <masterofones@outlook.com>
2016-04-08 15:08:44 +0100

    Created Base User Classes

---
+++ src/database.php
@@ -0,0 +1,12 @@
+<?php
+namespace ioawesome\userspace;
+
+class DatabaseManager {
+
+    /**
+     * Uses Doctrine ORM to create required db structure
+     */
+    public function install() {
+        
+    }
+}
